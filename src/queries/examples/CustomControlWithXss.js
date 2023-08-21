12f071ebf54c8cfd99d686d09bcc2df84d53b182
Jeongsoo Lee <jeongsoolee09@github.com>
2023-08-21 14:48:33 -0700

    Change HTML-like syntax to Markdown

--- src/queries/examples/CustomControlWithXss.js
+++ src/queries/examples/CustomControlWithXss.js
@@ -1,1 +1,13 @@
-// api version 2
+sap.ui.define(["sap/ui/core/Control"], function (Control) {
+    return Control.extend("vulnerable.control.xss", {
+        metadata: { properties: { text: { type: "string" } } },
+        renderer: {
+            apiVersion: 2,
+            render: function (oRm, oControl) {
+                oRm.openStart("div", oControl);
+                oRm.unsafeHtml(oControl.getText()); // sink
+                oRm.close("div");
+            }
+        }
+    });
+})
--- src/queries/examples/CustomControlWithXssRemoved.js
+++ src/queries/examples/CustomControlWithXssRemoved.js
@@ -1,4 +1,13 @@
-// Showcase all three:
-// 1. sanitized property
-// 2. encodeXML
-// 3. openStart, openEnd
+sap.ui.define(["sap/ui/core/Control", "sap/base/security/encodeXML"], function (Control, encodeXML) {
+    return Control.extend("vulnerable.control.xss", {
+        metadata: { properties: { text: { type: "int" } } }, // 1. constrain the type
+        renderer: {
+            apiVersion: 2,
+            render: function (oRm, oControl) {
+                oRm.openStart("div", oControl);
+                oRm.unsafeHtml(encodeXML(oControl.getText()); // 2. use security functions
+                oRm.close("div");
+            }
+        }
+    });
+})
