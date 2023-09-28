8d3ea62f1287399a7c62ffa29bd15a1d9c3d182a
Jeongsoo Lee <jeongsoolee09@github.com>
2023-09-28 15:02:27 -0700

    Add variants which uses TaggedTemplate instead of MethodCall and vice versa

--- cap/test/cql/select.js
+++ cap/test/cql/select.js
@@ -51,12 +51,30 @@ var select = SELECT.from`Table`.where("col1 in ('*', 10)");
 var select = SELECT.from`Table`.where`col1 in ${[("*", 10)]}`;
 var select = SELECT.from`Table`.where({ col1: 10, and: { col2: 11 } });
 
+var select = SELECT.from(Table).where({ col1: "*" });
+var select = SELECT.from(Table).where("col1='*'");
+var select = SELECT.from(Table).where`"col1='*'"`;
+var select = SELECT.from(Table).where("col1=", "*");
+var select = SELECT.from(Table).where`col = ${"*"}`;
+var select = SELECT.from(Table).where("col1 in ('*', 10)");
+var select = SELECT.from(Table).where`col1 in ${[("*", 10)]}`;
+var select = SELECT.from(Table).where({ col1: 10, and: { col2: 11 } });
+
 // .groupBy()
 var select = SELECT.from`Table`.groupBy("col1", "col2");
 var select = SELECT.from`Table`.groupBy`col1, col2`;
 var select = SELECT.from`Table`.groupBy("col1.prop1", "col2.prop2");
 var select = SELECT.from`Table`.groupBy`col1.prop1, col2.prop2`;
 var select = SELECT.from`Table`.groupBy(
+  { ref: ["col1", "prop1"] },
+  { ref: ["col2", "prop2"] }
+);
+
+var select = SELECT.from(Table).groupBy("col1", "col2");
+var select = SELECT.from(Table).groupBy`col1, col2`;
+var select = SELECT.from(Table).groupBy("col1.prop1", "col2.prop2");
+var select = SELECT.from(Table).groupBy`col1.prop1, col2.prop2`;
+var select = SELECT.from(Table).groupBy(
   { ref: ["col1", "prop1"] },
   { ref: ["col2", "prop2"] }
 );
@@ -70,6 +88,15 @@ var select = SELECT.from`Table`.having`col = ${"*"}`;
 var select = SELECT.from`Table`.having("col1 in ('*', 10)");
 var select = SELECT.from`Table`.having`col1 in ${[("*", 10)]}`;
 var select = SELECT.from`Table`.having({ col1: 10, and: { col2: 11 } });
+
+var select = SELECT.from(Table).having({ col1: "*" });
+var select = SELECT.from(Table).having("col1='*'");
+var select = SELECT.from(Table).having`"col1='*'"`;
+var select = SELECT.from(Table).having("col1=", "*");
+var select = SELECT.from(Table).having`col = ${"*"}`;
+var select = SELECT.from(Table).having("col1 in ('*', 10)");
+var select = SELECT.from(Table).having`col1 in ${[("*", 10)]}`;
+var select = SELECT.from(Table).having({ col1: 10, and: { col2: 11 } });
 
 var select = SELECT.from`Table`.groupBy("col1", "col2").having({ col1: "*" });
 var select = SELECT.from`Table`.groupBy("col1", "col2").having("col1='*'");
@@ -86,6 +113,21 @@ var select = SELECT.from`Table`
   .groupBy("col1", "col2")
   .having({ col1: 10, and: { col2: 11 } });
 
+var select = SELECT.from(Table).groupBy("col1", "col2").having({ col1: "*" });
+var select = SELECT.from(Table).groupBy("col1", "col2").having("col1='*'");
+var select = SELECT.from(Table).groupBy("col1", "col2").having`"col1='*'"`;
+var select = SELECT.from(Table).groupBy("col1", "col2").having("col1=", "*");
+var select = SELECT.from(Table).groupBy("col1", "col2").having`col = ${"*"}`;
+var select = SELECT.from(Table)
+  .groupBy("col1", "col2")
+  .having("col1 in ('*', 10)");
+var select = SELECT.from(Table).groupBy("col1", "col2").having`col1 in ${[
+  ("*", 10),
+]}`;
+var select = SELECT.from(Table)
+  .groupBy("col1", "col2")
+  .having({ col1: 10, and: { col2: 11 } });
+
 var select = SELECT.from`Table`.groupBy`col1, col2`.having({ col1: "*" });
 var select = SELECT.from`Table`.groupBy`col1, col2`.having("col1='*'");
 var select = SELECT.from`Table`.groupBy`col1, col2`.having`"col1='*'"`;
@@ -96,6 +138,20 @@ var select = SELECT.from`Table`.groupBy`col1, col2`.having`col1 in ${[
   ("*", 10),
 ]}`;
 var select = SELECT.from`Table`.groupBy`col1, col2`.having({
+  col1: 10,
+  and: { col2: 11 },
+});
+
+var select = SELECT.from`Table`.groupBy(col1, col2).having({ col1: "*" });
+var select = SELECT.from`Table`.groupBy(col1, col2).having("col1='*'");
+var select = SELECT.from`Table`.groupBy(col1, col2).having`"col1='*'"`;
+var select = SELECT.from`Table`.groupBy(col1, col2).having("col1=", "*");
+var select = SELECT.from`Table`.groupBy(col1, col2).having`col = ${"*"}`;
+var select = SELECT.from`Table`.groupBy(col1, col2).having("col1 in ('*', 10)");
+var select = SELECT.from`Table`.groupBy(col1, col2).having`col1 in ${[
+  ("*", 10),
+]}`;
+var select = SELECT.from`Table`.groupBy(col1, col2).having({
   col1: 10,
   and: { col2: 11 },
 });
@@ -184,13 +240,33 @@ var select = SELECT.from`Table`.orderBy`col1 desc, col2.prop2`;
 var select = SELECT.from`Table`.orderBy`col1.prop1, col2 desc`;
 var select = SELECT.from`Table`.orderBy`col1 desc col2 desc`;
 
+var select = SELECT.from(Table).orderBy`col1.prop1, col2.prop2`;
+var select = SELECT.from(Table).orderBy`col1 asc, col2.prop2`;
+var select = SELECT.from(Table).orderBy`col1.prop1, col2 asc`;
+var select = SELECT.from(Table).orderBy`col1 asc col2 asc`;
+var select = SELECT.from(Table).orderBy`col1.prop1, col2.prop2`;
+var select = SELECT.from(Table).orderBy`col1 desc, col2.prop2`;
+var select = SELECT.from(Table).orderBy`col1.prop1, col2 desc`;
+var select = SELECT.from(Table).orderBy`col1 desc col2 desc`;
+
 // .limit()
 var select = SELECT.from`Table`.limit(10);
+var select = SELECT.from`Table`.limit`${10}`;
 var select = SELECT.from`Table`.limit(10, 20);
 var select = SELECT.from`Table`.limit({ val: 10 });
 var select = SELECT.from`Table`.limit({ val: 10 }, { val: 20 });
 var select = SELECT.from`Table`.limit({ ref: ["limitVal"] });
 var select = SELECT.from`Table`.limit({
+  ref: [{ id: "function", args: { p: { ref: ["arg1"] } } }],
+});
+
+var select = SELECT.from(Table).limit(10);
+var select = SELECT.from(Table).limit`${10}`;
+var select = SELECT.from(Table).limit(10, 20);
+var select = SELECT.from(Table).limit({ val: 10 });
+var select = SELECT.from(Table).limit({ val: 10 }, { val: 20 });
+var select = SELECT.from(Table).limit({ ref: ["limitVal"] });
+var select = SELECT.from(Table).limit({
   ref: [{ id: "function", args: { p: { ref: ["arg1"] } } }],
 });
 
