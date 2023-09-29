c17958bf57c99ab26b0a42d497c7451ae5049b7c
Jeongsoo Lee <jeongsoolee09@github.com>
2023-09-29 15:22:34 -0700

    Fix syntactical mistakes on unit tests for SELECT

--- cap/test/cql/select.js
+++ cap/test/cql/select.js
@@ -44,7 +44,7 @@ var select = SELECT.from(Table).columns("col1", {
 // .where()
 var select = SELECT.from`Table`.where({ col1: "*" });
 var select = SELECT.from`Table`.where("col1='*'");
-var select = SELECT.from`Table`.where`"col1='*'"`;
+var select = SELECT.from`Table`.where`col1=${"*"}`;
 var select = SELECT.from`Table`.where("col1=", "*");
 var select = SELECT.from`Table`.where`col = ${"*"}`;
 var select = SELECT.from`Table`.where("col1 in ('*', 10)");
@@ -53,7 +53,7 @@ var select = SELECT.from`Table`.where({ col1: 10, and: { col2: 11 } });
 
 var select = SELECT.from(Table).where({ col1: "*" });
 var select = SELECT.from(Table).where("col1='*'");
-var select = SELECT.from(Table).where`"col1='*'"`;
+var select = SELECT.from(Table).where`col1=${"*"}`;
 var select = SELECT.from(Table).where("col1=", "*");
 var select = SELECT.from(Table).where`col = ${"*"}`;
 var select = SELECT.from(Table).where("col1 in ('*', 10)");
@@ -82,7 +82,7 @@ var select = SELECT.from(Table).groupBy(
 // .having()
 var select = SELECT.from`Table`.having({ col1: "*" });
 var select = SELECT.from`Table`.having("col1='*'");
-var select = SELECT.from`Table`.having`"col1='*'"`;
+var select = SELECT.from`Table`.having`col1=${"*"}`;
 var select = SELECT.from`Table`.having("col1=", "*");
 var select = SELECT.from`Table`.having`col = ${"*"}`;
 var select = SELECT.from`Table`.having("col1 in ('*', 10)");
@@ -91,7 +91,7 @@ var select = SELECT.from`Table`.having({ col1: 10, and: { col2: 11 } });
 
 var select = SELECT.from(Table).having({ col1: "*" });
 var select = SELECT.from(Table).having("col1='*'");
-var select = SELECT.from(Table).having`"col1='*'"`;
+var select = SELECT.from(Table).having`col1=${"*"}`;
 var select = SELECT.from(Table).having("col1=", "*");
 var select = SELECT.from(Table).having`col = ${"*"}`;
 var select = SELECT.from(Table).having("col1 in ('*', 10)");
@@ -100,7 +100,7 @@ var select = SELECT.from(Table).having({ col1: 10, and: { col2: 11 } });
 
 var select = SELECT.from`Table`.groupBy("col1", "col2").having({ col1: "*" });
 var select = SELECT.from`Table`.groupBy("col1", "col2").having("col1='*'");
-var select = SELECT.from`Table`.groupBy("col1", "col2").having`"col1='*'"`;
+var select = SELECT.from`Table`.groupBy("col1", "col2").having`col1=${"*"}`;
 var select = SELECT.from`Table`.groupBy("col1", "col2").having("col1=", "*");
 var select = SELECT.from`Table`.groupBy("col1", "col2").having`col = ${"*"}`;
 var select = SELECT.from`Table`
@@ -115,7 +115,7 @@ var select = SELECT.from`Table`
 
 var select = SELECT.from(Table).groupBy("col1", "col2").having({ col1: "*" });
 var select = SELECT.from(Table).groupBy("col1", "col2").having("col1='*'");
-var select = SELECT.from(Table).groupBy("col1", "col2").having`"col1='*'"`;
+var select = SELECT.from(Table).groupBy("col1", "col2").having`col1=${"*"}`;
 var select = SELECT.from(Table).groupBy("col1", "col2").having("col1=", "*");
 var select = SELECT.from(Table).groupBy("col1", "col2").having`col = ${"*"}`;
 var select = SELECT.from(Table)
@@ -130,7 +130,7 @@ var select = SELECT.from(Table)
 
 var select = SELECT.from`Table`.groupBy`col1, col2`.having({ col1: "*" });
 var select = SELECT.from`Table`.groupBy`col1, col2`.having("col1='*'");
-var select = SELECT.from`Table`.groupBy`col1, col2`.having`"col1='*'"`;
+var select = SELECT.from`Table`.groupBy`col1, col2`.having`col1=${"*"}`;
 var select = SELECT.from`Table`.groupBy`col1, col2`.having("col1=", "*");
 var select = SELECT.from`Table`.groupBy`col1, col2`.having`col = ${"*"}`;
 var select = SELECT.from`Table`.groupBy`col1, col2`.having("col1 in ('*', 10)");
@@ -144,7 +144,7 @@ var select = SELECT.from`Table`.groupBy`col1, col2`.having({
 
 var select = SELECT.from`Table`.groupBy(col1, col2).having({ col1: "*" });
 var select = SELECT.from`Table`.groupBy(col1, col2).having("col1='*'");
-var select = SELECT.from`Table`.groupBy(col1, col2).having`"col1='*'"`;
+var select = SELECT.from`Table`.groupBy(col1, col2).having`col1=${"*"}`;
 var select = SELECT.from`Table`.groupBy(col1, col2).having("col1=", "*");
 var select = SELECT.from`Table`.groupBy(col1, col2).having`col = ${"*"}`;
 var select = SELECT.from`Table`.groupBy(col1, col2).having("col1 in ('*', 10)");
@@ -163,7 +163,7 @@ var select = SELECT.from`Table`
   .groupBy("col1.prop1", "col2.prop2")
   .having("col1='*'");
 var select = SELECT.from`Table`.groupBy("col1.prop1", "col2.prop2")
-  .having`"col1='*'"`;
+  .having`col1=${"*"}`;
 var select = SELECT.from`Table`
   .groupBy("col1.prop1", "col2.prop2")
   .having("col1=", "*");
@@ -185,7 +185,7 @@ var select = SELECT.from`Table`.groupBy`col1.prop1, col2.prop2`.having(
   "col1='*'"
 );
 var select = SELECT.from`Table`.groupBy`col1.prop1, col2.prop2`
-  .having`"col1='*'"`;
+  .having`col1=${"*"}`;
 var select = SELECT.from`Table`.groupBy`col1.prop1, col2.prop2`.having(
   "col1=",
   "*"
@@ -211,7 +211,7 @@ var select = SELECT.from`Table`
 var select = SELECT.from`Table`.groupBy(
   { ref: ["col1", "prop1"] },
   { ref: ["col2", "prop2"] }
-).having`"col1='*'"`;
+).having`col1=${"*"}`;
 var select = SELECT.from`Table`
   .groupBy({ ref: ["col1", "prop1"] }, { ref: ["col2", "prop2"] })
   .having("col1=", "*");
