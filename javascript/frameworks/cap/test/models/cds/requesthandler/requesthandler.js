7ecbf4a3973401baa40ff6d428fd0c0f8b6ee4c4
Mauro Baluda <mbaluda@github.com>
2024-02-05 18:38:57 +0100

    Model event handler phases before and after. Add query help files.

--- javascript/frameworks/cap/test/models/cds/requesthandler/requesthandler.js
+++ javascript/frameworks/cap/test/models/cds/requesthandler/requesthandler.js
@@ -1,17 +1,17 @@
 const cds = require("@sap/cds");
 class BooksService extends cds.ApplicationService {
-    init(){
+    init() {
         const { Books, Authors } = this.entities
-        this.on ('READ',[Books,Authors], req => req.target.data) 
-        this.on ('UPDATE',Books, req => { 
-            let [ ID ] = req.params
-            return Object.assign (Books.data[ID], req.data)
+        this.on('READ', [Books, Authors], req => req.target.data)
+        this.on('UPDATE', Books, req => {
+            let [ID] = req.params
+            return Object.assign(Books.data[ID], req.data)
         })
-    return super.init()
+        return super.init()
     }
 }
 module.exports = BooksService
 
-cds.serve('./test-service').with ((srv) => {
-        srv.on ('READ','Books', (req) => req.reply([]))
-    })
+cds.serve('./test-service').with((srv) => {
+    srv.before('READ', 'Books', (req) => req.reply([]))
+})
