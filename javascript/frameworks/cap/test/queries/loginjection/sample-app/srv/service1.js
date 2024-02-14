f354b9d15df06b9f8493b97b813e3e8feb670cb3
Jeongsoo Lee <jeongsoolee09@github.com>
2024-02-13 17:02:15 -0800

    Add missing `init()` and add some comments

--- javascript/frameworks/cap/test/queries/loginjection/sample-app/srv/service1.js
+++ javascript/frameworks/cap/test/queries/loginjection/sample-app/srv/service1.js
@@ -2,8 +2,10 @@ const cds = require("@sap/cds");
 
 /* Emit a "Received1" event upon receiving a READ request on its entity. */
 module.exports = class Service1 extends cds.ApplicationService {
-    this.on("READ", "Entity1/Attribute1", (req) => {
-        const { messageToPass } = req.data;
-        await this.emit("Received1", { messageToPass });
-    });
+    init() {
+        this.on("READ", "Entity1/Attribute1", (req) => {
+            const { messageToPass } = req.data;
+            await this.emit("Received1", { messageToPass });
+        });
+    }
 }
--- javascript/frameworks/cap/test/queries/sqlinjection/sample-app/server.js
+++ javascript/frameworks/cap/test/queries/sqlinjection/sample-app/server.js
@@ -5,7 +5,7 @@ cds.once("bootstrap", (app) => {
     app.serve("/sql-injection").from("@advanced-security/log-injection");
 });
 
-/*  */
+/* Upon receiving "Received1" event emitted by Service1, make Service2 run a CQL query. */
 Service1.on("Received1", async (msg) => {
     const { messageToPass } = msg.data;
     await Service2.run(SELECT.from `Service2Entity`.where(`Attribute3=${messageToPass}`));
