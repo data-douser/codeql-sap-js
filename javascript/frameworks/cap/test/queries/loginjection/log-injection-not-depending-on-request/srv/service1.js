eb5b49ebc2a56f327db1eb16728e90f3d1f8bd69
Jeongsoo Lee <jeongsoolee09@github.com>
2024-03-20 14:27:06 -0700

    Fix `log-injection-not-depending-on-request`

--- javascript/frameworks/cap/test/queries/loginjection/log-injection-not-depending-on-request/srv/service1.js
+++ javascript/frameworks/cap/test/queries/loginjection/log-injection-not-depending-on-request/srv/service1.js
@@ -5,6 +5,7 @@ module.exports = class Service1 extends cds.ApplicationService {
     init() {
         this.on("send1", async (req) => { // req is not used at all
             let datetime = new Date();
+            const Service2 = await cds.connect.to("service-2");
             Service2.send("send2", { messageToPass: datetime.toString() });
         });
     }
--- javascript/frameworks/cap/test/queries/loginjection/log-injection-not-depending-on-request/srv/service2.js
+++ javascript/frameworks/cap/test/queries/loginjection/log-injection-not-depending-on-request/srv/service2.js
@@ -3,7 +3,7 @@ const LOG = cds.log("logger");
 
 module.exports = cds.service.impl(() => {
     /* Log upon receiving an "Received2" event. */
-    this.on("Received2", async (msg) => {
+    this.on("send2", async (msg) => {
         const { messageToPass } = msg.data;
         /* A log injection sink. */
         LOG.info("Received: ", messageToPass);  // CAP log injection alert
