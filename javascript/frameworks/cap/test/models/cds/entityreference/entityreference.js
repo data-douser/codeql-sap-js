15839c369e0ebea19d142e7c54775313c24918af
Jeongsoo Lee <jeongsoolee09@github.com>
2024-06-25 14:08:03 -0700

    Initial commit of `entityreference.js`

---
+++ javascript/frameworks/cap/test/models/cds/entityreference/entityreference.js
@@ -0,0 +1,5 @@
+/* ========== 1. Obtained from db.entities ========== */
+
+/* ========== 2. Obtained from this.entities ========== */
+
+/* ========== 3. Obtained from Fluent API TaggedTemplateExpr ========== */
