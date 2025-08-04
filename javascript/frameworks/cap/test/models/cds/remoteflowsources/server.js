71ada992c395e111724446310646dcc5d06ddd50
Jeongsoo Lee <jeongsoolee09@github.com>
2025-08-04 12:24:57 -0400

    Update server.js and check in expected results of `ExposedServices`
    - `server.js` had a reference to a service that doesn't exist. Made it point to Service1 instead.
    - Create expected results of `ExposedServices`, a complementary test to `RemoteFlowSource`.

--- javascript/frameworks/cap/test/models/cds/remoteflowsources/server.js
+++ javascript/frameworks/cap/test/models/cds/remoteflowsources/server.js
@@ -3,6 +3,6 @@ const app = require("express")();
 
 cds.serve("all").in(app);
 
-cds.serve('./some-service').with((srv) => {
+cds.serve('Service1').with((srv) => {
   srv.before('READ', 'Books', (req) => req.reply([])) // SAFE: Exposed service (fallback), but not a taint source
 })
