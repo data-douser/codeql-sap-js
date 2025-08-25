c39da05b686293816e65fd22acde4609034b817f
Jeongsoo Lee <jeongsoolee09@github.com>
2025-08-25 16:44:28 -0400

    Add more cases to `userdefinedservice`

--- javascript/frameworks/cap/test/models/cds/userdefinedservice/service2.js
+++ javascript/frameworks/cap/test/models/cds/userdefinedservice/service2.js
@@ -1,12 +1,5 @@
 const cds = require("@sap/cds");
 
-class BooksService extends cds.ApplicationService {
-  init() {
-    return super.init()
-  }
-}
-module.exports = BooksService
-
 module.exports = class LogService extends cds.Service {
   init() {
     return super.init()
