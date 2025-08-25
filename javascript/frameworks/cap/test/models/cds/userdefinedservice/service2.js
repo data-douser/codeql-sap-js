511d4986eed01d11eb1d4422441280b7e5b8f324
Jeongsoo Lee <jeongsoolee09@github.com>
2025-08-25 16:44:46 -0400

    Add additional changes to `userdefinedservice`

--- javascript/frameworks/cap/test/models/cds/userdefinedservice/service2.js
+++ javascript/frameworks/cap/test/models/cds/userdefinedservice/service2.js
@@ -1,6 +1,6 @@
 const cds = require("@sap/cds");
 
-module.exports = class LogService extends cds.Service {
+module.exports = class LogService extends cds.Service {  // UserDefinedService
   init() {
     return super.init()
   }
